ddf9e46f05995ae5080e526669b4242513ef6360
xirokami <Kiro.kind@mail.ru>
2026-05-14 13:40:25 +0300

    laptop update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,32 +1,56 @@
-if status is-interactive
-    set -g fish_greeting ''
-    set -g -x PIP_REQUIRE_VIRTUALENV true
-    alias n='nvim'
-    alias ls='lsd'
-    alias l='lsd -l'
-    alias la='lsd -A'
-    alias lla='lsd -la'
-    alias lt='lsd --tree'
-    alias zapret='sudo bash ~/Public/zapret/zapret-discord-youtube-linux/service.sh'
-    zoxide init fish | source
-    starship init fish | source
-end
+set -g fish_greeting ''
+set -gx EDITOR nvim
+
+zoxide init fish | source
+starship init fish | source
 
 function spf
-    set os $(uname -s)
-
-    if test "$os" = Linux
-        set spf_last_dir "$HOME/.local/state/superfile/lastdir"
-    end
-
-    if test "$os" = Darwin
-        set spf_last_dir "$HOME/Library/Application Support/superfile/lastdir"
-    end
-
+    set spf_last_dir "$HOME/.local/state/superfile/lastdir"
     command spf $argv
-
     if test -f "$spf_last_dir"
         source "$spf_last_dir"
         rm -f -- "$spf_last_dir" >>/dev/null
     end
 end
+
+alias n 'fg || nvim'
+alias q exit
+
+alias ls lsd
+alias l "ls -l"
+alias la "l -a"
+
+alias mkdir "mkdir -p"
+alias mv "mv -i"
+
+alias c cargo
+alias cn "cargo +nightly"
+alias zapret='sudo bash ~/Public/zapret/zapret-discord-youtube-linux/service.sh'
+
+abbr sc systemctl
+abbr jc journalctl
+
+abbr g git
+abbr ga "git add"
+abbr gb "git branch"
+abbr gch "git checkout"
+abbr gc "git commit -m"
+abbr gd "git diff"
+abbr gf "git fetch"
+abbr gl "git log"
+abbr gm "git merge"
+abbr gpull "git pull"
+abbr gpush "git push"
+abbr gr "git rebase"
+abbr grm "git remote"
+abbr gs "git status"
+abbr gsh "git show"
+abbr gst "git stash"
+
+# Fix emoji and others rendering
+set -g fish_emoji_width 2
+set -g fish_cursor_insert line
+set -g fish_cursor_replace_one underscore
+
+# Vi key bindings
+# set -g fish_key_bindings fish_vi_key_bindings
